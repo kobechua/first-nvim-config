0e4d8c2c4ca4a565db5e83b1f2a123957b3144f5
kobechua <kobechua14@gmail.com>
2024-06-12 12:35:40 -0400

    theme change

--- lua/kobe/plugins/colorscheme.lua
+++ lua/kobe/plugins/colorscheme.lua
@@ -2,7 +2,10 @@ return {
     "folke/tokyonight.nvim",
     priority = 1000,
     config = function()
-        vim.cmd("colorscheme tokyonight")
+        require("tokyonight").setup ({
+            style = moon
+        })
+        vim.cmd("colorscheme tokyonight-moon")
     end
 
 }
