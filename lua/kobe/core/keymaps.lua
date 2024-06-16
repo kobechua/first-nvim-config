cd44f8b7a444b3af435e34fafe874348b37e6f6b
kobechua <kobechua14@gmail.com>
2024-06-16 01:47:07 -0400

    autocomplete changes

--- lua/kobe/core/keymaps.lua
+++ lua/kobe/core/keymaps.lua
@@ -13,5 +13,4 @@ keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", {desc = "Open new tab"})
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", {desc = "Close tab"})
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", {desc = "Open current buffer in new tab"})
 
-keymap.set("n", "<leader>n", "<cmd>NvimTreeOpen<CR>", { desc = "Open NvimTree"})
 
